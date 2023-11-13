35b9af1b08bad18bad26b0b80d08d4e62a6a057a
k0yote <yjmk0yote@gmail.com>
2023-11-13 13:49:07 +0900

    modofied hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,15 @@
 
 module.exports = {
   networks: {
-      hardhat: {
-          forking: {
-              url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.SDK_ALCHEMY_KEY}`,
-          },
-          chainId: 1,
-          initialBaseFeePerGas: 0, // Allow 0 gas fees when testing
+    hardhat: {
+          // forking: {
+          //     url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.SDK_ALCHEMY_KEY}`,
+          // },
+          // chainId: 1,
+          // initialBaseFeePerGas: 0, // Allow 0 gas fees when testing
           throwOnTransactionFailures: true, // Brownie expects transactions to throw on revert
           throwOnCallFailures: true, // Brownie expects calls to throw on failure
+          allowUnlimitedContractSize: true,
      }
   }
 }
